6047916dabc510f8b29e9b7544487c073ce27cc3
GallaFrancesco <francesco.galla3@gmail.com>
2020-05-22 02:45:18 +0200

    bottino: fix irc tls connection

--- source/bottino/main.d
+++ source/bottino/main.d
@@ -2,11 +2,19 @@ module bottino.main;
 
 import ae.net.asockets;
 import ae.net.irc.client;
-
+import ae.net.shutdown;
+import ae.net.ssl;
+import ae.net.ssl.openssl;
+import ae.sys.log;
+import ae.sys.timing;
+import ae.utils.text;
 
 import std.algorithm.iteration;
 import std.string;
 import std.getopt;
+import std.typecons : No;
+
+debug import std.stdio;
 
 struct BotConfig {
 public:
@@ -23,31 +31,50 @@ public:
         if (ch.startsWith("#")) channels ~= '#' ~ ch;
         else channels ~= ch;
     }
+
+    inout bool initialized() @safe @nogc
+    {
+        return !server.empty && !nick.empty;
+    }
+
 }
+
+private alias NoTLS = No;
 
 /**
  * Connect to configured server
  */
-void botConnect(TcpConnection tcp, IrcClient irc, immutable BotConfig config)
+void botConnect(Conn, TLS)(Conn conn, IrcClient irc, BotConfig config, TLS tls)
+    if(is(TLS : SSLAdapter) || is(TLS == NoTLS))
 {
+    assert(config.initialized, "Cannot call botConnect with an empty config");
+
     irc.nickname = config.nick;
     irc.realname = config.realname;
-    tcp.connect(config.server, config.port);
+    irc.encoder = irc.decoder = &nullStringTransform;
+    irc.log = createLogger("IRC:"~config.server);
+    conn.connect(config.server, config.port);
 
-    config.channels.each!((channel) => irc.join(channel));
+    debug {
+        while(!irc.connected) {
+            writeln("not connected?");
+        }
+    }
+
+    // called upon successful connection
+    irc.handleConnect = () { config.channels.each!((ch) => irc.join(ch)); };
 }
 
 void main(string[] args)
 {
-    TcpConnection tcp;
-    IrcClient irc;
     BotConfig config;
-    string[] chs;
+    bool tls = false;
 
     auto helpInformation = getopt(
             args,
             "server|s", "Server address", &config.server,
             "port|p", "Server port [6667]", &config.port,
+            "tls|t", "Use TLS (requires a SSL provider)", &tls,
             "nick|n", "Bot nick", &config.nick,
             "realname|r", "Bot realname [bottino]", &config.realname,
             "channel|c", "Add a channel (might be specified more than once)", &config.channels);
@@ -56,5 +83,20 @@ void main(string[] args)
         defaultGetoptPrinter("Bottino: IRC bot",
                              helpInformation.options);
         return;
+    }
+
+    TcpConnection tcp = new TcpConnection;
+    IrcClient irc;
+
+    if(tls) {
+        debug writeln("Using TLS");
+        auto ctx = ssl.createContext(OpenSSLContext.Kind.client);
+        auto sslConn = ssl.createAdapter(ctx, tcp);
+        irc = new IrcClient(sslConn);
+        botConnect(tcp, irc, config, sslConn);
+    } else {
+        debug writeln("Not using TLS");
+        irc = new IrcClient(tcp);
+        botConnect(tcp, irc, config, NoTLS());
     }
 }
